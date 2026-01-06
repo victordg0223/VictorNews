539e6c638377d90e4fd4683a57c88a47f7e39a13
Victor D. Gonçalves <victordg0223@gmail.com>
2026-01-06 16:01:57 +0000

    add  and

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(requet, response) {
+import database from "../../../../infra/database.js";
+
+async function status(requet, response) {
+  const result = await database.query("SELECT 1+1 as sum;");
+  console.log(result.rows);
   response.status(200).json({ Chave: "Valor" });
 }
 
