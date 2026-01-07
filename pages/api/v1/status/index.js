7c03ea94a1ec38a54db7ec87bf4ca0462b7e5104
Victor D. Gonçalves <victordg0223@gmail.com>
2026-01-07 16:38:51 +0000

    add base para desafio dia 20

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,19 @@
 import database from "infra/database.js";
 
-async function status(requet, response) {
-  const result = await database.query("SELECT 1+1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ Chave: "Valor" });
+async function status(request, response) {
+  const updatedAt = new Date().toISOString();
+  const dbVersion = 0;
+  const serverHealth = 0;
+  const maxConnections = 0;
+  const openedConnections = 0;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    version: dbVersion,
+    status: serverHealth,
+    max_connections: maxConnections,
+    opened_connections: openedConnections,
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,14 @@
-test("GET to /api/v1/status returns status 200", async () => {
-  const responde = await fetch("http://localhost:3000/api/v1/status");
-  expect(responde.status).toBe(200);
+test("Valida data Status", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status"); // Faz a requisição para o endpoint de status
+  expect(response.status).toBe(200); // Verifica se o status da resposta é 200
+
+  const responseBody = await response.json(); // Converte a resposta para JSON
+  expect(responseBody.updated_at).toBeDefined(); // Verifica se o campo updated_at está definido
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString(); // Tenta converter o campo updated_at para uma data válida
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt); // Verifica se o campo updated_at é uma data válida
 });
+test("Valida versão Postgress", async () => {});
+test("Validar saúde do servidor", async () => {});
+test("Validar conexões máximas", async () => {});
+test("Validar conexões usadas", async () => {});
